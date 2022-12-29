594f1fd8405b204f4985ae6b4324118afc9a8605
Jon Bråte <jon.brate@ibv.uio.no>
2022-12-29 15:22:26 +0100

    Renaming blast out column headers

--- bin/blast_parse.R
+++ bin/blast_parse.R
@@ -17,10 +17,23 @@ agens      <- args[5]
 if (agens == "HCV") {
 
   scaf <- read_tsv(blast_out, col_names = FALSE) %>% 
+    # Rename columns
+    rename("qseqid" = "X1",
+      "sseqid" = "X2",
+      "pident" = "X3",
+      "length" = "X4",
+      "mismatch" = "X5",
+      "gapopen" = "X6",
+      "qstart" = "X7",
+      "qend" = "X8",
+      "sstart" = "X9",
+      "send" = "X10",
+      "evalue" = "X11",
+      "bitscore" = "X12") %>% 
     # Add a column for the genotype
-    separate(X2, into = c("genotype", NA), remove = FALSE) %>% 
+    separate(sseqid, into = c("genotype", NA), remove = FALSE) %>% 
     # Count the number of each subject
-    add_count(X2) 
+    add_count(sseqid) 
 
   # Which genotypes are present?
   genotypes <- scaf %>% 
