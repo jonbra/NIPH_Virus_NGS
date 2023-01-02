da4c6f04b3c4b864fcc9a508f3e08668f5ff17c7
jonbra <jon.brate@fhi.no>
2023-01-02 13:05:57 +0100

    Major changes

--- bin/create_samplesheet.R
+++ bin/create_samplesheet.R
@@ -1,66 +1,36 @@
+#!/usr/bin/env Rscript
+
 library(tidyverse)
 
-# Read Opportuni-C samples
-resultater <- readxl::read_xlsx("/mnt/N/Virologi/NGS/1-NGS-Analyser/3-Prosjekt/HCV-prosjekter/OpportuniC/2_Resultater_HCV_OpportuniC.xlsx",
-                                sheet = "Resultater",
-                                skip = 1)
+args = commandArgs(trailingOnly=TRUE)
 
-resultater <- readxl::read_xlsx("/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/HCV/2022/Resultater_HCV_oversikt_2022.xlsx",
-                                sheet = "Resultater",
-                                skip = 1)
-resultater <- resultater %>% 
-  rename("Sample_ID" = `Sample_ID\r\n(markert gult de som ikke har full resistensrapport)`)
+folder <- args[1] # "/mnt/N/NGS/3-Sekvenseringsbiblioteker/2022/Illumina_RunXXX/Run820_Virus/Run820/"
 
-# List all fastq files
-fastq_files_Run613 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2021/Illumina_RunXXX/Run613_Corona_HCV/",
-                               recursive = TRUE,
-                               full.names = TRUE,
-                               pattern = "gz$")
-fastq_files_Run622 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2021/Illumina_RunXXX/Run622/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_Run630 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2021/Illumina_RunXXX/Run630/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_Run652 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2021/Illumina_RunXXX/Run652/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_Run712 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2022/Illumina_RunXXX/Run712_Corona+HCV/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_Run726 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2022/Illumina_RunXXX/Run726_Corona_HCV_test_nye_indexer/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_Run738 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2022/Illumina_RunXXX/Run738_Corona_HCV/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_Run820 <- list.files("/mnt/N/NGS/3-Sekvenseringsbiblioteker/2022/Illumina_RunXXX/Run820_Virus/",
-                                 recursive = TRUE,
-                                 full.names = TRUE,
-                                 pattern = "gz$")
-fastq_files_all <- c(fastq_files_Run613, fastq_files_Run622, fastq_files_Run630, fastq_files_Run652, fastq_files_Run712, fastq_files_Run726, fastq_files_Run738)
-fastq_files_all <- fastq_files_Run820
+# Get the fastq files
+fastq <- list.files(folder,
+           recursive = TRUE,
+           full.names = TRUE,
+           pattern = "gz$")
 
-R1 <- sort(fastq_files_all[grep("R1", fastq_files_all)])
-R2 <- sort(fastq_files_all[grep("R2", fastq_files_all)])
+R1 <- sort(fastq[grep("R1", fastq)])
+R2 <- sort(fastq[grep("R2", fastq)])
 
 df <- as_tibble(cbind(R1, R2))
 
-df <- df %>%
-  mutate(sample_id = gsub("_.*", "", basename(R1))) %>%
-  select("sample" = sample_id,
-         "fastq_1" = R1,
-         "fastq_2" = R2)
+# Check that the R1 and R2 files are correctly paired
+tmp <- df %>% 
+  mutate(tmpR1 = gsub("_.*", "", basename(R1)),
+         tmpR2 = gsub("_.*", "", basename(R2))) %>% 
+  select(tmpR1, tmpR2)
 
-# Match with the result sheet 
-resultater_final <- left_join(resultater, df, by = c("Sample_ID" = "sample")) %>% 
-  drop_na(Sample_ID) %>% 
-  select("sample" = Sample_ID, fastq_1, fastq_2)
+if (identical(tmp$tmpR1, tmp$tmpR2)){
+  df <- df %>%
+    mutate(sample_id = gsub("_.*", "", basename(R1))) %>%
+    select("sample" = sample_id,
+           "fastq_1" = R1,
+           "fastq_2" = R2)
+} else {
+  print("R1 and R2 files not correctly paired")
+}
 
-write_csv(resultater_final, "/home/jonr/Prosjekter/learning_nextflow/2022.11.30-HCV_Run820_samplesheet.csv")
+write_csv(df, "/home/jonr/Prosjekter/learning_nextflow/HBV_Run820_samplesheet.csv")
