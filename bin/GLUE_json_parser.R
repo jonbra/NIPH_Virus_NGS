215fc9476c94a3cc8ea36db2197b8d63fed6874b
jonbra <jon.brate@fhi.no>
2023-04-26 14:01:27 +0200

    Added short names for mutations

--- bin/GLUE_json_parser.R
+++ bin/GLUE_json_parser.R
@@ -4,8 +4,8 @@ library(tidyverse)
 library(jsonlite)
 
 args <- commandArgs(trailingOnly = TRUE)
-if (length(args) < 1) {
-  stop("Usage: GLUE_json_parser.R <summary.csv>", call. = FALSE)
+if (length(args) < 2) {
+  stop("Usage: GLUE_json_parser.R <run name> <summary.csv>", call. = FALSE)
 }
 
 
@@ -21,31 +21,46 @@ df_final <- tibble(
   "Sample" = character(), 
   "glecaprevir" = character(), 
   "glecaprevir_mut" = character(), 
+  "glecaprevir_mut_short" = character(), 
   "grazoprevir" = character(), 
   "grazoprevir_mut" = character(), 
+  "grazoprevir_mut_short" = character(), 
   "paritaprevir" = character(), 
   "paritaprevir_mut" = character(), 
+  "paritaprevir_mut_short" = character(), 
   "voxilaprevir" = character(), 
   "voxilaprevir_mut" = character(), 
+  "voxilaprevir_mut_short" = character(), 
   "NS34A" = character(),
+  "NS34A_short" = character(),
   "daclatasvir" = character(), 
   "daclatasvir_mut" = character(), 
+  "daclatasvir_mut_short" = character(), 
   "elbasvir" = character(), 
   "elbasvir_mut" = character(), 
+  "elbasvir_mut_short" = character(), 
   "ledipasvir" = character(), 
-  "ledipasvir_mut" = character(), 
+  "ledipasvir_mut" = character(),
+  "ledipasvir_mut_short" = character(),
   "ombitasvir" = character(), 
-  "ombitasvir_mut" = character(), 
+  "ombitasvir_mut" = character(),
+  "ombitasvir_mut_short" = character(),
   "pibrentasvir" = character(),
   "pibrentasvir_mut" = character(),
+  "pibrentasvir_mut_short" = character(),
   "velpatasvir" = character(), 
   "velpatasvir_mut" = character(), 
+  "velpatasvir_mut_short" = character(), 
   "NS5A" = character(),
+  "NS5A_short" = character(),
   "dasabuvir" = character(),
   "dasabuvir_mut" = character(),
+  "dasabuvir_mut_short" = character(),
   "sofosbuvir" = character(),
   "sofosbuvir_mut" = character(),
+  "sofosbuvir_mut_short" = character(),
   "NS5B" = character(),
+  "NS5B_short" = character(),
   "HCV project version" = character(),
   "GLUE engine version" = character(),
   "PHE drug resistance extension version"  = character()
@@ -74,35 +89,50 @@ for (x in 1:length(json_files)) {
   
   # One row per sample
   # Create a temporary dataframe to populate
-  df <- as.data.frame(matrix(nrow = 1, ncol = 31))
+  df <- as.data.frame(matrix(nrow = 1, ncol = 46))
   colnames(df) <- c("Sample", 
                     "glecaprevir", 
-                    "glecaprevir_mut", 
+                    "glecaprevir_mut",
+                    "glecaprevir_mut_short",
                     "grazoprevir", 
                     "grazoprevir_mut", 
+                    "grazoprevir_mut_short", 
                     "paritaprevir", 
                     "paritaprevir_mut", 
+                    "paritaprevir_mut_short", 
                     "voxilaprevir", 
                     "voxilaprevir_mut", 
+                    "voxilaprevir_mut_short", 
                     "NS34A",
+                    "NS34A_short",
                     "daclatasvir", 
                     "daclatasvir_mut", 
+                    "daclatasvir_mut_short", 
                     "elbasvir", 
                     "elbasvir_mut", 
+                    "elbasvir_mut_short", 
                     "ledipasvir", 
                     "ledipasvir_mut", 
+                    "ledipasvir_mut_short", 
                     "ombitasvir", 
                     "ombitasvir_mut", 
+                    "ombitasvir_mut_short", 
                     "pibrentasvir",
                     "pibrentasvir_mut",
+                    "pibrentasvir_mut_short",
                     "velpatasvir", 
                     "velpatasvir_mut", 
+                    "velpatasvir_mut_short", 
                     "NS5A",
+                    "NS5A_short",
                     "dasabuvir",
                     "dasabuvir_mut",
+                    "dasabuvir_mut_short",
                     "sofosbuvir",
                     "sofosbuvir_mut",
+                    "sofosbuvir_mut_short",
                     "NS5B",
+                    "NS5B_short",
                     "HCV project version",
                     "GLUE engine version",
                     "PHE drug resistance extension version")
@@ -126,27 +156,29 @@ for (x in 1:length(json_files)) {
                 
                 # De tre kategoriene er lister. Hvis lengden er > 0 betyr det at det er en mutasjon i den
                 mut <- vector(mode = "character") # Create empty vector to hold mutations
+                mut_short <- vector(mode = "character") # Create empty vector to hold mutations
                 if (length(json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_I"]]) > 0) {
                   for (n in 1:length(json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_I"]])) {
                     mut <- c(mut, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_I"]][[n]][["displayStructure"]])
+                    mut_short <- c(mut_short, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_I"]][[n]][["structure"]])
                   }
-                  #df[[paste0(genes_drugs[[l]][m], "_mut")]] <- json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_I"]][[1]][["displayStructure"]]
                 } 
                 if (length(json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_II"]]) > 0) {
                   for (n in 1:length(json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_II"]])) {
                     mut <- c(mut, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_II"]][[n]][["displayStructure"]])
+                    mut_short <- c(mut_short, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_II"]][[n]][["structure"]])
                   }
-                  #mut <- c(mut, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_II"]][[1]][["displayStructure"]])
-                  #df[[paste0(genes_drugs[[l]][m], "_mut")]] <- json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_II"]][[1]][["displayStructure"]]
                 } 
                 if (length(json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_III"]]) > 0) {
                   for (n in 1:length(json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_III"]])) {
                     mut <- c(mut, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_III"]][[n]][["displayStructure"]])
+                    mut_short <- c(mut_short, json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_III"]][[n]][["structure"]])
                   }
-                  #df[[paste0(genes_drugs[[l]][m], "_mut")]] <- json[["phdrReport"]][["samReferenceResult"]][["drugScores"]][[i]][["drugAssessments"]][[k]][["rasScores_category_III"]][[1]][["displayStructure"]]
                 }
                 mut <- paste(mut, collapse = ";")
+                mut_short <- paste(mut_short, collapse = ";")
                 df[[paste0(genes_drugs[[l]][m], "_mut")]] <- mut
+                df[[paste0(genes_drugs[[l]][m], "_mut_short")]] <- mut_short
               }
             }
           }
@@ -161,15 +193,18 @@ for (x in 1:length(json_files)) {
   tmp <- tmp %>% 
     unite("NS34A", c(glecaprevir_mut, grazoprevir_mut, paritaprevir_mut, voxilaprevir_mut), sep = ";", na.rm = TRUE) %>% 
     unite("NS5A", c(daclatasvir_mut, elbasvir_mut, ledipasvir_mut, ombitasvir_mut, pibrentasvir_mut, velpatasvir_mut), sep = ";", na.rm = TRUE) %>% 
-    unite("NS5B", c(dasabuvir_mut, sofosbuvir_mut), sep = ";", na.rm = TRUE) 
+    unite("NS5B", c(dasabuvir_mut, sofosbuvir_mut), sep = ";", na.rm = TRUE) %>% 
+    unite("NS34A_short", c(glecaprevir_mut_short, grazoprevir_mut_short, paritaprevir_mut_short, voxilaprevir_mut_short), sep = ";", na.rm = TRUE) %>% 
+    unite("NS5A_short", c(daclatasvir_mut_short, elbasvir_mut_short, ledipasvir_mut_short, ombitasvir_mut_short, pibrentasvir_mut_short, velpatasvir_mut_short), sep = ";", na.rm = TRUE) %>% 
+    unite("NS5B_short", c(dasabuvir_mut_short, sofosbuvir_mut_short), sep = ";", na.rm = TRUE) 
   
   # Gjøre om innholdet i cellene til en vector, deretter fjerne dupliater i vektoren
   try(df$NS34A <- paste(unique(unlist(strsplit(gsub(" ", "", unlist(strsplit(unlist(strsplit(tmp$NS34A, ";")), ","))), "\\+"))), collapse = ";"))
-  #try(df$NS34A <- unique(unlist(strsplit(tmp %>% pull(NS34A), ";"))))
   try(df$NS5A <- paste(unique(unlist(strsplit(gsub(" ", "", unlist(strsplit(unlist(strsplit(tmp$NS5A, ";")), ","))), "\\+"))), collapse = ";"))
-  #try(df$NS5A <- unique(unlist(strsplit(tmp %>% pull(NS5A), ";"))))
   try(df$NS5B <- paste(unique(unlist(strsplit(gsub(" ", "", unlist(strsplit(unlist(strsplit(tmp$NS5B, ";")), ","))), "\\+"))), collapse = ";"))
-  #try(df$NS5B <- unique(unlist(strsplit(tmp %>% pull(NS5B), ";"))))
+  try(df$NS34A_short <- paste(unique(unlist(strsplit(gsub(" ", "", unlist(strsplit(unlist(strsplit(tmp$NS34A_short, ";")), ","))), "\\+"))), collapse = ";"))
+  try(df$NS5A_short <- paste(unique(unlist(strsplit(gsub(" ", "", unlist(strsplit(unlist(strsplit(tmp$NS5A_short, ";")), ","))), "\\+"))), collapse = ";"))
+  try(df$NS5B_short <- paste(unique(unlist(strsplit(gsub(" ", "", unlist(strsplit(unlist(strsplit(tmp$NS5B_short, ";")), ","))), "\\+"))), collapse = ";"))
   
   df <- as_tibble(df)
   
@@ -178,11 +213,12 @@ for (x in 1:length(json_files)) {
 }
 
 # Kamillas script: Må binde df_final til summary-fila igjen
-summary <- read_tsv(args[1]) %>% # summary <- read_tsv("/home/jonr/Prosjekter/learning_nextflow/json-test/Run837_HCV_summaries_v7sort.tsv")
+run_name <- args[1]
+summary <- read_tsv(args[2]) %>% # summary <- read_tsv("/home/jonr/Prosjekter/learning_nextflow/json-test/Run837_HCV_summaries_v7sort.tsv")
   rename("Sample" = "Parameters:")
 
 # join the data
 summary_final <- left_join(summary, df_final)
 
-write_tsv(summary_final, file = "summary_with_glue.tsv")
+write_tsv(summary_final, file = paste0(run_name, "_summary_with_glue.tsv"))
 
