28fcc7907e70341d1bd0f77ae0672eb2c3d0d212
jonbra <jon.brate@fhi.no>
2023-06-02 11:19:16 +0200

    Printing scaffold lengths

--- bin/summarize_mapping.R
+++ bin/summarize_mapping.R
@@ -5,6 +5,7 @@ library(tidyverse)
 # Number of mapped reads --------------------------------------------------
 path_1 <- "stats/"
 path_2 <- "depth/"
+path_3 <- "blast/"
 
 df <- list.files(path = path_1, pattern = "stats", full.names = TRUE) %>% 
   # Keep the file names as the names of the list elements
@@ -38,14 +39,12 @@ write_csv(df, file = "Genotype_mapping_summary_wide.csv")
 
 # Coverage ----------------------------------------------------------------
 
-# Thoughts: Calculate the number of sites with a coverage at least 5
-
 # List files
 files <- list.files(path = path_2, pattern = "gz", full.names = TRUE)
 
 # Empty df
 tmp_df <- as.data.frame(matrix(nrow = length(files), ncol = 3))
-colnames(tmp_df) <- c("sampleName", "reference", "cov_breadth")
+colnames(tmp_df) <- c("sampleName", "reference", "cov_breadth_min_5")
 
 for (i in 1:length(files)) {
   
@@ -69,7 +68,7 @@ for (i in 1:length(files)) {
   
   # Coverage breadth
   breadth <- round(pos / ref_length * 100, digits = 2)
-  tmp_df$cov_breadth[i] <- breadth
+  tmp_df$cov_breadth_min_5[i] <- breadth
   
 }
 
@@ -77,14 +76,49 @@ for (i in 1:length(files)) {
 tmp_df <- as_tibble(tmp_df)
 tmp_df <- tmp_df %>% 
   separate(reference, into = c("genotype", NA), sep = "_", remove = FALSE) %>% 
-  select(sampleName, genotype, reference, cov_breadth)
+  select(sampleName, genotype, reference, cov_breadth_min_5)
 
 # Add the number of mapped reads
 # NB: Temporary cleaning of sample names
 tmp <- df %>% 
   mutate(sampleName = basename(sampleName)) %>% 
-  pivot_longer(!sampleName, names_to = "reference", values_to = "count") %>% filter(!is.na(count))
+  pivot_longer(!sampleName, names_to = "reference", values_to = "mapped_reads") %>% filter(!is.na(mapped_reads))
 tmp_df <- left_join(tmp_df, tmp)
+
+
+# Length of scaffolds -----------------------------------------------------
+
+# Print the length of the longest scaffold matching the given reference
+blast_files <- list.files(path = path_3, pattern = "out$", full.names = TRUE)
+
+# Empty df
+df <- as.data.frame(matrix(nrow = length(files), ncol = 2))
+colnames(df) <- c("sampleName", "scaffold_length")
+
+for (i in 1:length(blast_files)) {
+  
+  # Read the length of scaffolds
+  blast_out <- read_tsv(blast_files[i], col_names = FALSE) %>% 
+    # Separate the genotype from the subject header
+    separate(X2, into = c("genotype", NA), remove = FALSE) %>% 
+    # Get scaffold length info
+    separate(X1, c(NA, NA, NA, "sc_length", NA, NA), sep = "_", remove = FALSE) %>% 
+    mutate(sc_length = as.numeric(sc_length)) %>% 
+    # Select the row with the longest scaffold lengths for each genotype/blast hit
+    group_by(genotype) %>% 
+    slice_max(order_by = sc_length, n = 1) %>% 
+    ungroup %>% 
+    # Sometimes the same scaffolds has two or more hits
+    distinct(X1, .keep_all = TRUE) %>% 
+    select(sc_length,
+           "reference" = X2) %>% 
+    # Legg til en kolonne med Sample Name
+    add_column("sampleName" = str_split(basename(blast_files[i]), "_")[[1]][1])
+  
+  # Add to tmp_df
+  tmp_df <- left_join(tmp_df, blast_out)
+
+}
 
 # Write file
 write_csv(tmp_df, file = "Genotype_mapping_summary_long.csv")
